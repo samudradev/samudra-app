e254049648c0dfe4726a897422adc0922fd4f511
Thaza_Kun <61819672+Thaza-Kun@users.noreply.github.com>
2023-07-22 21:45:00 +0800

    ✨ NEW: Export structs to ts class using ts_rs

--- src-tauri/src-database/src/data/export.rs
+++ src-tauri/src-database/src/data/export.rs
@@ -1,18 +1,37 @@
+// This macro is written as such to accomodate the ts_rs export macro
+// which apparently does not allow the Option enum to be passed into a macro.
+// Doing so would raise `unreachable!()`.
+// It needs to be defined inside this macro_rules.
+// Refer https://github.com/Aleph-Alpha/ts-rs/issues/83
+// (I have tried the suggestion to use qualified full path `std::option::Option` resulting in the same error.)
 macro_rules! export {
-    {$dataclass:ident from $datamodel:ident
-        {
-            $($field:ident : $type:ty),*;
-            $($tagfield:ident : $tagmodel:ident => ..$tagclass:ident),*
-        }
+    {
+        $dataclass:ident from $datamodel:ident with {
+            $($field:ident : $type:ty),*
+            $(;rename $old_field:ident to $new_field:ident: $renamed:ty),*
+            $(;optional {
+                $($optfield:ident : $opttype:ty),*
+            })?
+            $(;attachment {
+                $($tagfield:ident : $tagmodel:ident => ..$tagclass:ident),*
+            })?
+    }
     } => {
-        #[derive(Debug, Serialize, Deserialize, Default)]
+        #[derive(Debug, Serialize, Deserialize, Default, TS)]
+        #[ts(export, export_to="../../src/bindings/")]
         pub struct $dataclass {
             $(
                 pub $field: $type,
             )*
             $(
-                $tagfield: Option::<Vec::<$tagclass>>,
+                pub $new_field: $renamed,
             )*
+            $($(
+                pub $optfield: Option::<$opttype>,
+            )*)?
+            $($(
+                pub $tagfield: Option::<Vec::<$tagclass>>,
+            )*)?
         }
         impl From<$datamodel> for $dataclass {
             fn from(value: $datamodel) -> Self {
@@ -20,11 +39,14 @@ macro_rules! export {
                     $(
                         $field: value.$field,
                     )*
+                    $(
+                        $new_field: <$renamed>::from(value.$old_field.unwrap()),
+                    )*
                     ..Default::default()
                 }
             }
         }
-        $(impl From<($datamodel, Vec<$tagmodel>)> for $dataclass {
+        $($(impl From<($datamodel, Vec<$tagmodel>)> for $dataclass {
             fn from(value: ($datamodel, Vec<$tagmodel>)) -> Self {
                 let mut lem = $dataclass::from(value.0.clone());
                 lem.$tagfield = Some(
@@ -36,6 +58,6 @@ macro_rules! export {
                 );
                 lem
             }
-        })*
+        })*)?
     };
 }
