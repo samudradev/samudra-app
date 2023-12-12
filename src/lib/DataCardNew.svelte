e1918cc391ed157601effe4f9e6c3f45080a9eaa
Thaza_Kun <61819672+Thaza-Kun@users.noreply.github.com>
2023-12-12 17:15:31 +0800

    ⚙ FIX: Insert cakupan dan kata_asing

--- src/lib/DataCardNew.svelte
+++ src/lib/DataCardNew.svelte
@@ -3,11 +3,16 @@
     import _ from "lodash";
     import type { KonsepItem } from "../bindings/KonsepItem";
     import { invoke } from "@tauri-apps/api";
+    import type { KataAsingItem } from "../bindings/KataAsingItem";
 
     let data: LemmaItem = {};
-    let konseps: KonsepItem[] = [];
+    $: konseps = [];
     let new_keterangan: string;
     let new_golongan_kata: string;
+    $: new_cakupans = [];
+    let new_cakupan: string;
+    $: new_kata_asings = [];
+    let new_kata_asing: KataAsingItem = { nama: "", bahasa: "" };
 
     function append_new_konsep() {
         konseps.push(
@@ -15,11 +20,29 @@
                 id: null,
                 keterangan: new_keterangan,
                 golongan_kata: new_golongan_kata,
-                cakupans: [],
-                kata_asing: [],
+                cakupans: new_cakupans,
+                kata_asing: new_kata_asings,
             }),
         );
+        konseps = konseps; // To force reload
+        new_cakupans = [];
+        new_kata_asings = [];
         new_keterangan = "";
+    }
+    function append_new_cakupan() {
+        new_cakupans.push(new_cakupan);
+        new_cakupans = new_cakupans; // To force reload
+        new_cakupan = "";
+    }
+    function append_new_kata_asing() {
+        new_kata_asings.push(new_kata_asing);
+        new_kata_asings = new_kata_asings; // To force reload
+        new_kata_asing = { nama: "", bahasa: "" };
+    }
+
+    function append_to_field(item: KonsepItem, field: string, value: string) {
+        item[field].push(value);
+        konseps = konseps;
     }
 
     async function insert_lemma() {
@@ -49,17 +72,59 @@
             </button>
         </div>
         <div class="text-left">
-            [+] {" "}
-            <select class="select" bind:value={new_golongan_kata}>
-                <option value="NAMA">NAMA</option>
-            </select>
             <form on:submit|preventDefault={append_new_konsep}>
-                <textarea class="textarea" bind:value={new_keterangan} />
+                [+] {" "}
+                <select class="select" bind:value={new_golongan_kata}>
+                    <option value="NAMA">NAMA</option>
+                </select>
                 <button type="submit">+</button>
+                <textarea class="textarea" bind:value={new_keterangan} />
+            </form>
+            <form on:submit|preventDefault={append_new_cakupan}>
+                <div class="label">
+                    <span class="label-text-alt">Cakupan</span>
+                </div>
+                <span class="join">
+                    <input
+                        type="text"
+                        placeholder="cakupan"
+                        class="textarea join-item"
+                        bind:value={new_cakupan}
+                    />
+                    <button type="submit" class="join-item">+</button>
+                </span>
+                {#each new_cakupans as cakupan}
+                    <div>{cakupan}</div>
+                {/each}
+            </form>
+            <form
+                on:submit|preventDefault={append_new_kata_asing}
+                class="form-control"
+            >
+                <div class="label">
+                    <span class="label-text-alt">Kata asing</span>
+                </div>
+                <span class="join">
+                    <input
+                        type="text"
+                        placeholder="kata"
+                        class="textarea join-item"
+                        bind:value={new_kata_asing.nama}
+                    />
+                    <input
+                        type="text"
+                        placeholder="bahasa"
+                        class="textarea join-item"
+                        bind:value={new_kata_asing.bahasa}
+                    />
+                    <button type="submit" class="join-item">+</button>
+                </span>
+                {#each new_kata_asings as kata_asing}
+                    <div>{kata_asing.nama}: {kata_asing.bahasa}</div>
+                {/each}
             </form>
         </div>
-        <!-- TODO reactively appear after previous insertion -->
-        <!-- {#each konseps as konsep, i}
+        {#each konseps as konsep, i}
             <div class="text-left">
                 {i + 1}.
                 <span class="badge">{konsep.golongan_kata}</span>
@@ -71,7 +136,7 @@
                                 <div class="badge">{cakupan}</div>
                             {/each}
                         </div>
-                    {:else}{/if}
+                    {/if}
                     {#if konsep.kata_asing != null}
                         <div class="column">
                             {#each konsep.kata_asing as kata_asing}
@@ -83,6 +148,6 @@
                     {/if}
                 </div>
             </div>
-        {/each} -->
+        {/each}
     </div>
 </div>
