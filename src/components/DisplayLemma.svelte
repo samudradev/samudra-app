652de3443e1b8fed9382df9664e367cab39e231d
Thaza_Kun <61819672+Thaza-Kun@users.noreply.github.com>
2023-12-15 18:20:19 +0800

    ✨ NEW: Export image

---
+++ src/components/DisplayLemma.svelte
@@ -0,0 +1,13 @@
+<script>
+    import DisplayKonseps from "./DisplayKonseps.svelte";
+    export let data;
+</script>
+
+<div class="card card-normal m-4 bg-base-100 shadow-xl">
+    <div class="card-body">
+        <div class="grid w-full grid-flow-row-dense grid-cols-4 grid-rows-1">
+            <h2 class="card-title col-span-3">{data.lemma}</h2>
+        </div>
+        <DisplayKonseps konseps={data.konseps} />
+    </div>
+</div>
--- src/lib/DataCardDisplay.svelte
+++ src/lib/DataCardDisplay.svelte
@@ -1,23 +1,68 @@
 <script lang="ts">
     import type { LemmaItem } from "../bindings/LemmaItem";
     import DisplayKonseps from "../components/DisplayKonseps.svelte";
+    import { toPng } from "html-to-image";
+
+    import { BaseDirectory, writeBinaryFile } from "@tauri-apps/api/fs";
+    import DisplayLemma from "../components/DisplayLemma.svelte";
 
     export let data: LemmaItem;
 
     export let toggle_edit;
+    let alert;
+    let filepath: string = ".";
+
+    const convertDataURIToBinary = (dataURI: string) =>
+        Uint8Array.from(window.atob(dataURI.replace(/^data[^,]+,/, "")), (v) =>
+            v.charCodeAt(0),
+        );
+
+    function export_png() {
+        alert.showModal();
+        let node = document.getElementById(data.lemma);
+        filepath = `./Samudra/export/${data.lemma}-${
+            new Date().toISOString().split("T")[0]
+        }.png`;
+        toPng(node).then(async (data_url) => {
+            writeBinaryFile(filepath, convertDataURIToBinary(data_url), {
+                dir: BaseDirectory.Document,
+            });
+        });
+    }
 </script>
 
-<div class="card card-normal m-4 w-96 bg-base-100 shadow-xl">
-    <div class="card-body">
-        <div class="grid w-full grid-flow-row-dense grid-cols-4 grid-rows-1">
-            <h2 class="card-title col-span-3">{data.lemma}</h2>
-            <button
-                class="btn btn-ghost btn-xs col-span-1"
-                on:click={toggle_edit}
-            >
-                Edit
-            </button>
-        </div>
-        <DisplayKonseps konseps={data.konseps} />
+<div class="relative w-[30em]" id={data.lemma}>
+    <div class="inset-0 w-full">
+        <DisplayLemma {data} />
+    </div>
+
+    <div class="menu menu-vertical absolute -right-[6em] top-0 py-[1em]">
+        <button class="my-[0.2em]" on:click={toggle_edit}> Edit </button>
+        <button class="my-[0.2em]" on:click={export_png}> Export </button>
     </div>
 </div>
+
+<dialog class="modal fixed bottom-0" bind:this={alert}>
+    <div role="alert" class="modal-box bg-success flex space-between space-x-2">
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="stroke-current shrink-0 h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            ><path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+            /></svg
+        >
+        <span>Image exported to $DOCUMENT{filepath.substring(1)}</span>
+    </div>
+    <form method="dialog" class="modal-backdrop transparent">
+        <button
+            on:click={() => {
+                alert.close();
+            }}
+        ></button>
+    </form>
+</dialog>
