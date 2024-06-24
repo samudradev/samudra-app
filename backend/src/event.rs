c96abcd5ed39102781f80c840dce96be25026c70
Thaza_Kun <61819672+Thaza-Kun@users.noreply.github.com>
2024-06-24 22:53:38 +0800

    👩‍💻 Menu Events as enum

--- backend/src/event.rs
+++ backend/src/event.rs
@@ -1,10 +1,7 @@
-use tauri::WindowMenuEvent;
+use tauri::{Runtime, WindowMenuEvent};
+
+use crate::menu::{DatabaseMenuItem, MenuItem};
 
 pub fn on_menu_event(event: WindowMenuEvent) {
-    match event.menu_item_id() {
-        // "register_database" => handle_database_registration(event),
-        eventname => {
-            event.window().emit(eventname, None::<String>).unwrap();
-        }
-    }
+    _ = DatabaseMenuItem::on_menu_event(&event)
 }
